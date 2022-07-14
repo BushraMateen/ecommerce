86bb33514da6b3f0c995d2d2d308517a71d4bbd2
BushraMateen <bushra.mateen95@outlook.com>
2022-07-14 09:57:44 +0530

    added functionality to cart

--- store/views.py
+++ store/views.py
@@ -1,5 +1,8 @@
+from argparse import Action
 from django.shortcuts import render
+from django.http import JsonResponse
 from .models import *
+import json
 
 def store(request):
     products = Product.objects.all()
@@ -27,5 +30,21 @@ def checkout(request):
         order = {'get_cart_total':0, 'get_cart_items':0}
     context = {'items':items,'order':order}
     return render(request, 'store/checkout.html',context)
+
+
+def updateItem(request):
+    data = json.loads(request.body)
+    productId = data['productId']
+    action = data['action']
+
+    print('Action:',action)
+    print('productId:',productId)
+
+    customer = request.user.customer
+    product = product.objects.get(id = product)
+    order, created = Order.objects.get_or_create(customer=customer,complete = False)
+    orderItem, created = OrderItem.objects.get_or_create(order=order,product = product)
+
+    return JsonResponse('Item was added',safe=False)
 
 
